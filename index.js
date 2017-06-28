16db6db00f16b01bfb1108a0a51b12602b94f1e1
Tawni Glover <equinoxjenkins@gmail.com>
2017-06-27 20:26:45 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var port = 3000;
 
+app.listen(port, function() {
+  console.log('listening on port', port)
+})
+
+//listen on the port
+
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+
+//use modules
+
+var messages = [];
+
+app.get('/messages', function (req, res, next) {
+  res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function (req, res, next) {
+  messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({ messages: messages });
+});
